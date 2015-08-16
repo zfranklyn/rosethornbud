a0f1e19a3fb711ccdbf97d726a9df57d361bbc0b
zfranklyn <zfranklyn@gmail.com>
2015-08-15 23:16:17 -0700

    added nonreusable template for posts. next steps need to work on getting handlebars to work, as well as adding icons to each post

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,33 @@
 (function(window, document, undefined) {
 
+	var postDeck = document.getElementById("content");
+	var postTemplate = document.getElementById("post-template")
+
+    var templates = {
+        renderPosts: Handlebars.compile(postTemplate.innerHTML)
+    };
 
 
 
 
 
+
+    function getStoredPosts() {
+        if (!localStorage.posts) {
+            // default to empty array
+            localStorage.posts = JSON.stringify([]);
+        }
+
+        return JSON.parse(localStorage.posts);
+    }
+
+    function storePosts(posts) {
+        localStorage.posts = JSON.stringify(posts);
+    }
+
+    function postGenerator(){
+    	// postDeck.innerHTML = templates.renderPosts({})
+    }
 
 
 
